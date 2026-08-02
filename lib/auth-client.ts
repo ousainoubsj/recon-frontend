e735388f833441d07b30939c5fe696261a20aab5
Ousainou BS Jammeh <ousainoubsj@gmail.com>
2026-08-02 19:10:53 +0000

    feat: add fetchOptions to authClient for improved credential handling

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,6 +3,9 @@ import { emailOTPClient, organizationClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  fetchOptions: {
+    credentials: "include",
+  },
   plugins: [organizationClient(), emailOTPClient()],
 })
 
